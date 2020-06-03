8ca2a4f24ddd929caba3f70e71080b9440acce45
Melvin Bentinger <melvinbentinger2@gmail.com>
2020-06-03 10:56:21 +0200

    happy little clouds

---
+++ Digitalt_Skapande/Assets/Scripts/cloud.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] cloudSprites;
+    SpriteRenderer thisRend;
+    float speed;
+    void Start()
+    {
+        thisRend = GetComponent<SpriteRenderer>();
+        thisRend.sprite = cloudSprites[Random.Range(0, cloudSprites.Length)];
+        if (Random.Range(1,3) == 1)
+        {
+            thisRend.flipX = true;
+        }
+        speed = Random.Range(0.5f,1.5f);
+        thisRend.sortingOrder = (int)(speed * 1000);
+    }
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (transform.localPosition.x > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Digitalt_Skapande/Assets/Scripts/cloud_generator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud_generator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cloud;
+    [SerializeField]
+    float cloudRate;
+    void Start()
+    {
+        makeHappyLittleCloud();
+        fillSky();
+    }
+    void Update()
+    {
+        
+    }
+    void makeHappyLittleCloud()
+    {
+        Vector2 startPosition;
+        startPosition.x = -10;
+        startPosition.y = Random.Range(-1f, 6f);
+        Transform newCloud = Instantiate(cloud, transform.position, Quaternion.identity).transform;
+        newCloud.parent = transform;
+        newCloud.localPosition = startPosition;
+        Invoke("makeHappyLittleCloud", Random.Range(1f, cloudRate));
+    }
+    void fillSky()
+    {
+        int i = 0;
+        while (i < 50)
+        {
+            Vector2 startPosition;
+            startPosition.x = Random.Range(-11f, 11f); ;
+            startPosition.y = Mathf.Pow(Random.Range(0, 3f),2) - 3;
+            Transform newCloud = Instantiate(cloud, transform.position, Quaternion.identity).transform;
+            newCloud.parent = transform;
+            newCloud.localPosition = startPosition; 
+            i++;
+        }
+    }
+}
