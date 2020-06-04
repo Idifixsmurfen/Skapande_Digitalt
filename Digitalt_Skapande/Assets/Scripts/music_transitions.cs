4db1379cf73cef0360bda09d7655c73b1c7768d9
Melvin Bentinger <melvinbentinger2@gmail.com>
2020-06-04 11:09:42 +0200

    Revert "don't break"
    This reverts commit e51840b2a6e787db710c37bf10f79d48f5fe0930.

--- Digitalt_Skapande/Assets/Scripts/music_transitions.cs
+++ Digitalt_Skapande/Assets/Scripts/music_transitions.cs
@@ -1,43 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class music_transitions : MonoBehaviour
 {
     [SerializeField]
     AudioClip[] clips;
     AudioSource source;
-    public int desiredClip;
-    void Start()
+    public int desiredMusic;
+    void Awake()
     {
         source = GetComponent<AudioSource>();
-        
+        InvokeRepeating("transition", 5.647f, 5.647f);
     }
     void Update()
     {
-        if (SceneManager.GetActiveScene().buildIndex > 1)
+       
+    }
+    void transition()
+    {
+        if (source.clip != clips[desiredMusic])
         {
-            if (GameObject.Find("Frog") != null)
-            {
-                desiredClip = 1;
-                if (GameObject.Find("Frog").GetComponent<Helth>().Hellth <= 1)
-                {
-                    desiredClip = 2;
-                }
-            }
-            else
-            {
-                desiredClip = 2;
-            }
-        }
-        else
-        {
-            desiredClip = 0;
-        }
-        if (!source.isPlaying)
-        {
-            source.clip = clips[desiredClip];
+            source.clip = clips[desiredMusic];
             source.Play();
         }
     }
